17bd2c1cf7d402aa8c3f72e823b34dc0445c348b
Minhyeok2487 <repeater2487@naver.com>
2021-01-30 21:02:08 +0900

    Update Clock

--- clock.js
+++ clock.js
@@ -1,6 +1,7 @@
 const clockContainer = document.querySelector(".js_clock");
 const mainContainer = document.querySelector(".main");
 const secondContainer = document.querySelector(".second");
+const ampmContainer = document.querySelector(".ampm");
 
 
 const clockTitle = clockContainer.querySelector("span");
@@ -10,9 +11,13 @@ const clockTitle = clockContainer.querySelector("span");
 function getTime(){
   const date = new Date();
   const minutes = date.getMinutes();
-  const hours = date.getHours();
+  var hours = date.getHours();
+  var ampm = hours>= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
   const seconds = date.getSeconds();
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+  ampmContainer.innerText = `${ampm}`;
   secondContainer.innerText = `${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
